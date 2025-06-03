bed93e7e57043f710a994ee641663b7d40660905
kacpersobierski <kacper.sobierski@rekordoswiata.pl>
2025-06-03 23:33:01 +0200

    Fix: remove incorrect duplicate paths from index

---
+++ backend/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(3001);
+}
+bootstrap();
