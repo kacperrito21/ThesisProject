be60b82320605d05cdd7a5f7cb7e9bc3c1de10fe
kacpersobierski <kacper.sobierski@rekordoswiata.pl>
2025-07-27 01:03:26 +0200

    fix(deploy): cookies fix

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -52,7 +52,7 @@ export class AuthController {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      sameSite: process.env.SAME_SITE,
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     res.status(200).json({ success: true });
